0be123db68cd4a811041fbb0b02486f66a9931a3
Joel Bech <122796634+JoBch@users.noreply.github.com>
2023-11-21 12:58:28 +0100

    Samma som förra men kommentarer tillagda

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,11 +7,17 @@ import java.awt.event.ActionListener;
 import static java.lang.Integer.parseInt;
 
 public class TicTacToe extends JFrame {
+    //TextField som outputar meddelanden till spelare
     static JTextField outputTF;
+    //Andreas? hehehe
     static int gameMode = 0; //använd variabeln eller likande för gamemode
+    //Padding för att knapparna skall se mer ordnade ut
     static final int UNI_PADDING = 10;
+    //Array för att plocka in värden på våra knappar
     static JButton [] arrayJB =  new JButton[9];
+    //För att få igång if-satser som byter vilken spelare som spelar - byt till bättre namn på denna!!
     static boolean startGame;
+    //Vårt spel
     private static TicTacToe application;
 
 
@@ -39,7 +45,7 @@ public class TicTacToe extends JFrame {
         panelJB.setLayout(gl);
 
         //Textfield för att displaya vems tur det är och så vidare.
-        outputTF = new JTextField("Här kommer det komma grejer");
+        outputTF = new JTextField("Welcome players!");
         Font bigFont = outputTF.getFont().deriveFont(Font.PLAIN, 30f);
         outputTF.setFont(bigFont);
         JPanel panelTop = new JPanel();
@@ -48,7 +54,7 @@ public class TicTacToe extends JFrame {
         application.add(panelTop, BorderLayout.NORTH);
 
         //For-loop som bygger våra JButtons
-        for (int i = 0; i < 9; i++) {
+        for (int i=0;i<9;i++) {
             Font f = new Font("Open sans", Font.BOLD, 50);
             arrayJB[i] = new JButton();
             arrayJB[i].addActionListener(listener);
@@ -60,6 +66,7 @@ public class TicTacToe extends JFrame {
         application.setVisible(true);
         //changeGameMode();
     }
+    //ActionListener till våra knappar för att kunna köra spelet
     static ActionListener listener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -69,26 +76,25 @@ public class TicTacToe extends JFrame {
                     if(startGame){
                         if (arrayJB[i].getText()=="") {
                             arrayJB[i].setText("O");
-                            outputTF.setText("X turn");
+                            outputTF.setText("X's turn");
                             startGame=false;
                             //Här måste vi bygga klart metoden för att kolla om X/O har vunnit, förlorat eller fått slut på drag
                             checkWinOrDraw();
                         }
                     }else { if (arrayJB[i].getText()==""){
                             arrayJB[i].setText("X");
-                            outputTF.setText("O turn");
+                            outputTF.setText("O's turn");
                             startGame=true;
                             checkWinOrDraw();
                         }
                     }
                 }
             }
-
+            //För att kolla så vår knapp funkar, plocka bort när vi är klara!!!
             if (e.getSource() instanceof JButton) {
                 String text = e.getActionCommand();
                 int i = parseInt(text);
                 System.out.println("Knappen funkar");
-
             }
         }
     };
