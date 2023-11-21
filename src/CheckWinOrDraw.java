c4015aa14fb54e15c197a394f9e554d1988b7387
SamosTheWiseSage <104500429+SamosTheWiseSage@users.noreply.github.com>
2023-11-21 10:53:56 +0100

    Update CheckWinOrDraw.java
    this is the basic setup of a way to end the game

--- src/CheckWinOrDraw.java
+++ src/CheckWinOrDraw.java
@@ -1,2 +1,25 @@
 public class CheckWinOrDraw {
+  // everything here will be changed by me to fit your guys design once we talk. 
+  
+  
+  // int counter ska ligga på rad 12
+      static int counter = 0;
+// counter ska ligga på rad 58 precis ovanför snoop ifsatsen
+  counter++;
+ //if satsen ska ligga på rad 69
+  if (counter == 9){
+         JDialog d = new JDialog(application, "dialog Box");
+
+         // creates a label for the dialog box
+         JLabel l = new JLabel("this is a Game over. Lets see who won");
+
+         d.add(l);
+
+         // setsize of dialog, we can decide the size later
+         d.setSize(100, 100);
+
+         // set visibility of dialog
+         d.setVisible(true);
+         
+     }
 }
